9f90cc0a447e5097367e09ed1bdb597f0cfe6556
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-03-12 23:00:43 -0300

    criação da cena Resultado e lógica para mostrar moeda
    criação da cena Resultado e lógica para mostrar moeda

--- index.android.js
+++ index.android.js
@@ -5,6 +5,7 @@ import { Router, Scene } from 'react-native-router-flux';
 import Principal from './src/components/Principal';
 import SobreJogo from './src/components/SobreJogo';
 import OutrosJogos from './src/components/OutrosJogos';
+import Resultado from './src/components/Resultado';
 
 export default class CaraOuCoroa extends Component {
   render() {
@@ -14,6 +15,7 @@ export default class CaraOuCoroa extends Component {
         <Scene key='principal' component={Principal} initil title="Cara ou Coroa" />
         <Scene key='sobrejogo' component={SobreJogo} title="Sobre o Jogo" />
         <Scene key='outrosjogos' component={OutrosJogos} title="Outros Jogos" />
+        <Scene key='resultado' component={Resultado} title="Resultado" />
       </Router>
     );
   }
